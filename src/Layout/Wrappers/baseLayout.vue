40be41b05375f9c509312c41126518fb4b43bced
Mahim Anjum Uchchhash <mauchchhash@gmail.com>
2022-02-06 22:52:36 +0600

    Removed Footer component from the baselayout

--- src/Layout/Wrappers/baseLayout.vue
+++ src/Layout/Wrappers/baseLayout.vue
@@ -11,9 +11,6 @@
                 <div class="app-main__inner">
                     <slot></slot>
                 </div>
-                <transition name="fade" mode="out-in" appear>
-                    <Footer />
-                </transition>
             </div>
             <!-- Modal Component -->
             <b-modal id="modal1" title="Bootstrap-Vue">
@@ -105,7 +102,6 @@
 <script>
 import Header from "../Components/Header";
 import Sidebar from "../Components/Sidebar";
-import Footer from "../Components/Footer";
 import VuePerfectScrollbar from "vue-perfect-scrollbar";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -118,7 +114,6 @@ export default {
     components: {
         Header,
         Sidebar,
-        Footer,
         VuePerfectScrollbar,
         "font-awesome-icon": FontAwesomeIcon,
     },
